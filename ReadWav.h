948d138b107a685cdfd70af576e43e56e8e0cbb3
aviets <70541912+aviets@users.noreply.github.com>
2021-05-04 14:44:55 -0700

    Update ReadWav.h

--- ReadWav.h
+++ ReadWav.h
@@ -15,10 +15,26 @@ public:
 	Reader ();
 	Reader (FILE*);
 
+/**
+* Takes in a FILE pointer of the audio data and displays the information
+@param the audio wav data as a FILE Pointer
+*/
 	void ReadWav (FILE*);
+
+/**
+* Reads in a FILE pointer to get the size fo the wav file
+@param FILE pointer to determine size of audio file
+*/
 	int getFileSize(FILE* inFile);
 
+/**
+* The function returns the buffer after being called
+*/
     unsigned char *getBuffer();
+
+/**
+* The function calls in the WavHeader struct and pulls the bits per sample variable to determine buffer type
+*/
     int getBufferSize() const;
 };
 
